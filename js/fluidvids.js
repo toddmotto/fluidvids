92a10b878e1dc6e0a69772ae1d6d6f6ed9c52402
Todd Motto <todd@toddmotto.com>
2013-06-20 21:39:35 +0100

    FluidVids v1.1.0

--- js/fluidvids.js
+++ js/fluidvids.js
@@ -1,50 +1,71 @@
-/*
-	Fluid and Responsive YouTube/Vimeo Videos v1.0.0
-	By Todd Motto: http://www.toddmotto.com
-	Latest version: https://github.com/toddmotto/fluidvids
-	
-	Modified 20 June 2013 by Jayden Seric (http://jaydenseric.com) to 
-	prevent original video iframe widths from being exceeded.
-	
-	Copyright 2013 Todd Motto
-	Licensed under the MIT license
-	http://www.opensource.org/licenses/mit-license.php
+/*!
+ *	FluidVids.js - fluid and responsive videos v1.1.0
+ *	by Todd Motto: http://toddmotto.com
+ *	Latest version: https://github.com/toddmotto/fluidvids
+ *	
+ *	Copyright 2013 Todd Motto. MIT licensed.
+ */
 
-	A raw JavaScript alternative to FitVids.js, fluid width video embeds
-*/ 
-(function() {
-	var iframes = document.getElementsByTagName('iframe');
-	
-	for (var i = 0; i < iframes.length; ++i) {
-		var iframe = iframes[i];
-		var players = /www.youtube.com|player.vimeo.com/;
-		if(iframe.src.search(players) !== -1) {
-			var width = iframe.width;
-			var height = iframe.height;
-			var videoRatio = (height / width) * 100;
+(function ( window, document, undefined ) {
+
+	/*
+	 * Grab all iframes on the page or return
+	 */
+	var iframes = document.getElementsByTagName( 'iframe' );
+
+	/*
+	 * Loop through the iframes array
+	 */
+	for ( var i = 0; i < iframes.length; i++ ) {
+
+		var iframe = iframes[i],
+
+		/*
+	     * RegExp, extend this if you need more players
+	     */
+		players = /www.youtube.com|player.vimeo.com/;
+
+		/*
+		 * If the RegExp pattern exists within the current iframe
+		 */
+		if ( iframe.src.search( players ) > 0 ) {
+
+			/*
+			 * Calculate the video ratio based on the iframe's w/h dimensions
+			 */
+			var videoRatio        = ( iframe.height / iframe.width ) * 100;
 			
+			/*
+			 * Replace the iframe's dimensions and position
+			 * the iframe absolute, this is the trick to emulate
+			 * the video ratio
+			 */
 			iframe.style.position = 'absolute';
-			iframe.style.top = '0';
-			iframe.style.left = '0';
-			iframe.width = '100%';
-			iframe.height = '100%';
+			iframe.style.top      = '0';
+			iframe.style.left     = '0';
+			iframe.width          = '100%';
+			iframe.height         = '100%';
 			
-			var inner = document.createElement('div');
-			inner.className = 'video-inner-wrapper';
-			inner.style.width = '100%';
-			inner.style.position = 'relative';
-			inner.style.paddingTop = videoRatio + '%';
+			/*
+			 * Wrap the iframe in a new <div> which uses a
+			 * dynamically fetched padding-top property based
+			 * on the video's w/h dimensions
+			 */
+			var wrap              = document.createElement( 'div' );
+			wrap.className        = 'video-wrap';
+			wrap.style.width      = '100%';
+			wrap.style.position   = 'relative';
+			wrap.style.paddingTop = videoRatio + '%';
 			
-			var parentNode = iframe.parentNode;
-			parentNode.insertBefore(inner, iframe);
-			inner.appendChild(iframe);
-			
-			var outer = document.createElement('div');
-			outer.className = 'video-outer-wrapper';
-			outer.style.maxWidth = width + 'px';
-			
-			parentNode.insertBefore(outer, inner);
-			outer.appendChild(inner);
+			/*
+			 * Add the iframe inside our newly created <div>
+			 */
+			var iframeParent      = iframe.parentNode;
+			iframeParent.insertBefore( wrap, iframe );
+			wrap.appendChild( iframe );
+
 		}
+
 	}
-})();
+
+})( window, document );
